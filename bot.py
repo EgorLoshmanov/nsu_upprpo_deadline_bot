ac0b7c6d82521f7f31aec55306fd04e4482c6042
Egor Loshmanov <e.loshmanov@g.nsu.ru>
2026-04-16 00:08:42 +0700

    Add .env support for BOT_TOKEN configuration

--- bot.py
+++ bot.py
@@ -4,9 +4,10 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from dotenv import load_dotenv
 
+load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
-
 dp = Dispatcher()
 
 
